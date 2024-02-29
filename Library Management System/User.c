556ecb4e7bd05fac85bb74c3067985ded7360a7c
Damienb123 <119611108+Damienb123@users.noreply.github.com>
2024-02-29 13:26:03 -0600

    Update User.c

--- Library Management System/User.c
+++ Library Management System/User.c
@@ -1,4 +1,4 @@
-// User.c
+//Creation of User.c
 #include <stdio.h>
 #include <string.h>
 #include "User.h"
@@ -8,7 +8,7 @@ void initializeUser(User* user, int id, const char* name, const char* contact) {
     strncpy(user->name, name, sizeof(user->name));
     strncpy(user->contact, contact, sizeof(user->contact));
 }
-
+//getting information to display user needed contents
 void displayUser(const User* user) {
     printf("ID: %d\nName: %s\nContact: %s\n", user->id, user->name, user->contact);
 }
